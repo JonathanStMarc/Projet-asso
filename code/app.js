073eb3780763bf983f6d380840043b58abd21198
jonathansttmarc <jonathanstmarcpgmail.com>
2022-06-21 19:42:01 +0200

    add modal connexion and modal contact

---
+++ code/app.js
@@ -0,0 +1,9 @@
+// $(function ($) {
+//     let formulaireContact = $("#formulaireContact");
+//     formulaireContact.on("click", function (e) {
+//         console.log(
+//             "vous avez ouvert une modale pour le formulaire de contact",
+//         );
+//         e.preventDefault();
+//     });
+// });
